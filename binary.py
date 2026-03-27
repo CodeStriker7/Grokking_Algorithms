cf630ab04faf372dd0bdd990ab07695dbaa6bc61
CodeStriker7 <djozod51@gmail.com>
2026-03-27 22:36:59 +0500

    Algorithms

--- binary.py
+++ binary.py
@@ -20,3 +20,11 @@ target = guess
 obj = Solution()
 solve = obj.searchbinary(nums, target)
 print (solve)
+
+# Recap
+# • Binary search is a lot faster than simple search.
+# • O(log n) is faster than O(n), but it gets a lot faster once the list of
+# items you’re searching through grows.
+# • Algorithm speed isn’t measured in seconds.
+# • Algorithm times are measured in terms of growth of an algorithm.
+# • Algorithm times are written in Big O notation.
